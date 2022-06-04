a72542a4fe24ab8f59b1c789b2975b326bcfe5bd
Gabriel Hirano <gomes.hirano@gmail.com>
2022-06-03 22:15:26 -0300

    add project

---
+++ Assets/Hero.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+
+    public float Speed;
+    public float JumpForce;
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Move();
+
+
+    }
+    void Move(){
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.localPosition += movement * Time.deltaTime * Speed;
+    }
+
+    void Jump(){
+        if(Input.GetButtonDown("Jump")){
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
