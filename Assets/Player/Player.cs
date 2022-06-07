5271c85876b25e8ad83f89ffc75cee437e44f494
Gabriel Hirano <gomes.hirano@gmail.com>
2022-06-06 23:28:48 -0300

    first animations

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator anim;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement =new Vector3(Input.GetAxis("Horizontal"), 0f ,0f);
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Speed", movement.magnitude);
+
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+    }
+}
